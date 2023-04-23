45d381ad56c06b7a078ffe6ef12ef36be3700d9d
jHhoseph988 <131305638+jHhoseph988@users.noreply.github.com>
2023-04-23 23:03:43 +0200

    nuevo docuemnto
    se agrego una linea de codigo

---
+++ HOLAMUNDO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author 57301
+ */
+public class HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello que tal mi nombre es joseph!");
+        System.out.println("que tal");
+
+          //esto rd u comnetario 
+        }
+}
